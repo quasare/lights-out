70286203e4b2a8032aa12f2723d6f40fd112f118
Thomas Adams <tqsare@gmail.com>
2019-07-17 10:10:02 -0700

    Updates

---
+++ precache-manifest.a7d39467dbdff67e9875ed27ef99293b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cc5b55a4753d848057b",
+    "url": "/lights-out/static/js/runtime~main.5cc5b55a.js"
+  },
+  {
+    "revision": "e3e40f62075928f9f0c3",
+    "url": "/lights-out/static/js/main.e3e40f62.chunk.js"
+  },
+  {
+    "revision": "207e5d056e3105dcde38",
+    "url": "/lights-out/static/js/1.207e5d05.chunk.js"
+  },
+  {
+    "revision": "e3e40f62075928f9f0c3",
+    "url": "/lights-out/static/css/main.209588f2.chunk.css"
+  },
+  {
+    "revision": "479df8aad545dc3e1cf50c9b141cba82",
+    "url": "/lights-out/index.html"
+  }
+];
